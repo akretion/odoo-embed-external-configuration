73f9213681a205b8b096ee5ece524f8086e0f8de
Sébastien BEAU <sebastien.beau@akretion.com>
2014-11-13 10:58:14 +0100

    [REF] refactor the tacking explode feature

--- production_from_configuration/stock.py
+++ production_from_configuration/stock.py
@@ -15,34 +15,48 @@ from osv import orm
 class StockMove(orm.Model):
     _inherit = 'stock.move'
 
+    #Complicated code that explode the prodlot number
+    #base function are really ugly, hard to inherit
     def _action_explode(self, cr, uid, move, context=None):
-        processed_ids = super(StockMove, self)._action_explode(
-            cr, uid, move, context=context)
-        child_ids = list(processed_ids)
-        move_master_id = child_ids.pop()
-        move_master = self.browse(cr, uid, move_master_id, context=context)
-        if move_master.product_id.track_from_sale:
-            lot_index = 1
-            for move in self.browse(cr, uid, child_ids, context=context):
-                if move.product_id.track_from_sale:
-                    vals = self._prepare_lot_for_move(
-                        cr, uid, move, move_master, lot_index, context=context)
-                    prodlot_id = self.pool['stock.production.lot'].create(
-                        cr, uid, vals, context=context)
-                    lot_index += 1
-                    print vals, prodlot_id
-                    move.write({'prodlot_id': prodlot_id})
-        return processed_ids
+        if context is None:
+            context = {}
+        ctx = context.copy()
+        ctx['prodlot_base_name'] = move.prodlot_id.name
+        ctx['prodlot_index'] = 1
+        ctx['explode_prodlot'] = True
+
+        return super(StockMove, self)._action_explode(
+            cr, uid, move, context=ctx)
+
+    def _prepare_explode_move(self, cr, uid, move, line, context=None):
+        product_obj = self.pool['product.product']
+        prodlot_obj = self.pool['stock.production.lot']
+        if context is None:
+            context = {}
+        res = super(StockMove, self).\
+            _prepare_explode_move(cr, uid, move, line, context=context)
+        if context.get('explode_prodlot'):
+            if move.product_id.track_from_sale:
+                product = product_obj.browse(
+                    cr, uid, line['product_id'], context=context)
+                if product.track_from_sale:
+                    prodlot_vals = self._prepare_lot_for_move(
+                        cr, uid, line['product_id'], move, context['prodlot_index'],
+                        context=context)
+                    res['prodlot_id'] = prodlot_obj.create(
+                        cr, uid, prodlot_vals, context=context)
+                    context['prodlot_index'] += 1
+        return res
 
     def _prepare_lot_for_move(
-            self, cr, uid, move, move_master, lot_index, context=None):
+            self, cr, uid, product_id, move, lot_index, context=None):
         lot_number = "%s-%03d" % (
-            move_master.sale_line_id.prodlot_id.name, lot_index)
+            move.prodlot_id.name, lot_index)
         return {
             'name': lot_number,
-            'product_id': move.product_id.id,
-            'company_id': move_master.company_id.id,
-            'config': move_master.prodlot_id.config,
+            'product_id': product_id,
+            'company_id': move.company_id.id,
+            'config': move.prodlot_id.config,
         }
 
     def create_chained_picking(self, cr, uid, moves, context=None):
