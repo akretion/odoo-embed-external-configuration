48b23b12f57fd740d8eaf3508feff722c7584b8d
david.beal@akretion.com <david.beal@akretion.com>
2014-12-12 10:25:59 +0100

    [ADD] production_customize

---
+++ production_customize/production.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  licence AGPL version 3 or later
+#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  Copyright (C) 2014 Akretion (http://www.akretion.com).
+#  @author David BEAL <david.beal@akretion.com>
+#
+##############################################################################
+
+from openerp import models
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    def _format_note_in_manuf_order(self, cr, uid, product, context=None):
+        super(MrpProduction, self)._format_note_in_manuf_order(
+            cr, uid, product, context=context)
+        return "Product: %s  Qty: %s" % (
+               product.name, self._product_config_dict[product.id]['qty'])
