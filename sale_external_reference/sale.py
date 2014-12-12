90e4edc4427ba0ac2ab271ebf2b07b3ac5fc715f
david.beal@akretion.com <david.beal@akretion.com>
2014-12-12 09:16:14 +0100

    [ADD] sale_external_reference

---
+++ sale_external_reference/sale.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  licence AGPL version 3 or later
+#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  Copyright (C) 2014 Akretion (http://www.akretion.com).
+#  @author Adrien CHAUSSENDE <adrien.chaussende@akretion.com>
+#  @author David BEAL <david.beal@akretion.com>
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    external_ref = fields.Char(string="External Reference")
